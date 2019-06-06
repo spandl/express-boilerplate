6933bfa9720d9baf4d7a5fbe0ab09750713cfcf3
Ralph Spandl <ralph@r42.ca>
2019-06-06 11:50:33 -0400

    Added methods to reduce and combine photo data in single file.

--- api/helpers/unsplash.js
+++ api/helpers/unsplash.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 require('isomorphic-fetch');
 const path = require('path');
 const axios = require('axios');
@@ -13,6 +14,9 @@ const requestURL = 'https://api.unsplash.com/';
 
 dotenv.config();
 
+// TO DO : split into several files
+// Place variables in environment variables or pass with function (maxLoop)
+
 const unsplash = new Unsplash({
     applicationId: process.env.UNSPLASH_applicationId,
     secret: process.env.UNSPLASH_secret,
@@ -21,8 +25,72 @@ const unsplash = new Unsplash({
 const maxLoop = 0;
 
 class UnsplashHelper {
+    static reducePhotoJSON() {
+        this.readFileNames('photos/').then((array) => {
+            // Make sure only valid files are processed
+            const result = array.filter(filename => filename.includes(fileNameStub));
+            const promises = [];
+            const photoJSON = [];
+
+            result.forEach((file) => {
+                promises.push(
+                    this.reduceJSON(`${folder}photos/${file}`).then((obj) => {
+                        photoJSON.push(obj);
+                    }),
+                );
+            });
+
+            Promise.all(promises).then(() => {
+                // Save JSON Array as single file
+                jsonfile.writeFile(`${folder}unsplash-d3.json`, photoJSON, (err) => {
+                    if (err) console.error(err);
+                });
+            });
+        });
+    }
+
+    static reduceJSON(theFile) {
+        return new Promise((resolve, reject) => {
+            jsonfile
+                .readFile(theFile)
+                .then((obj) => {
+                    // Do your work of copying only nodes that are required
+                    const reducedJSON = {};
+
+                    reducedJSON.id = obj.id;
+                    reducedJSON.created_at = obj.created_at;
+                    reducedJSON.updated_at = obj.updated_at;
+                    reducedJSON.width = obj.width;
+                    reducedJSON.height = obj.height;
+                    reducedJSON.color = obj.color;
+                    reducedJSON.description = obj.description;
+                    reducedJSON.urls = obj.urls;
+                    reducedJSON.exif = obj.exif;
+                    reducedJSON.location = obj.location;
+                    reducedJSON.tags = obj.tags;
+                    reducedJSON.sponsored = obj.sponsored;
+                    reducedJSON.sponsored_by = obj.sponsored_by;
+                    reducedJSON.sponsored_impressions_id = obj.sponsored_impressions_id;
+                    reducedJSON.likes = obj.likes;
+                    reducedJSON.views = obj.views;
+                    reducedJSON.downloads = obj.downloads;
+
+                    // User data
+                    reducedJSON.user = {};
+                    reducedJSON.user.id = obj.user.id;
+                    reducedJSON.user.username = obj.user.username;
+                    reducedJSON.user.first_name = obj.user.first_name;
+                    reducedJSON.user.last_name = obj.user.last_name;
+                    reducedJSON.user.location = obj.user.location;
+
+                    resolve(reducedJSON);
+                })
+                .catch(error => reject(error));
+        });
+    }
+
     static getPhotoInfoFromFileList() {
-        this.readFileNames().then((array) => {
+        this.readFileNames('').then((array) => {
             const result = array.filter(filename => filename.includes(fileNameStub));
             const photoIdArray = [];
             const promises = [];
@@ -73,7 +141,7 @@ class UnsplashHelper {
         });
         Promise.all(promises).then(() => {
             // GET ALL JSON PHOTO SFROM UNSPLASH
-            console.log('All JSON should start downloading in the /photos folder');
+            console.log('All JSON should be downloaded in the /photos folder');
         });
     }
 
@@ -93,11 +161,11 @@ class UnsplashHelper {
         });
     }
 
-    static readFileNames() {
+    static readFileNames(subfolder) {
         const fileArray = [];
 
         return new Promise((resolve, reject) => {
-            fs.readdir(folder, (err, files) => {
+            fs.readdir(folder + subfolder, (err, files) => {
                 files.forEach((file) => {
                     fileArray.push(file);
                 });
--- api/routes/download.js
+++ api/routes/download.js
@@ -4,14 +4,37 @@ const router = express.Router();
 const unsplashHelper = require('../helpers/unsplash');
 
 router.get('/', (req, res, next) => {
-    res.render('download', { title: 'Download Data from Unsplash', data: unsplashHelper.getPhotoInfoFromFileList() }, (err, html) => {
-        if (err) {
-            next(err);
-        } else {
-            res.send(html);
-            res.end();
-        }
-    });
+    res.render(
+        'download',
+        { title: 'Home page to 4 endpoints', data: 'disabled' },
+        (err, html) => {
+            // unsplashHelper.getPhotoInfoFromFileList()
+            if (err) {
+                next(err);
+            } else {
+                res.send(html);
+                res.end();
+            }
+        },
+    );
 });
+
+
+router.get('/getPhotoList', (req, res) => {
+    res.render('download', { title: 'Reduce all Photo JSON files to one JSON', data: 'getPhotoList: disabled' });
+});
+
+router.get('/getPhotoInfoFromFileList', (req, res) => {
+    res.render('download', { title: 'Reduce all Photo JSON files to one JSON', data: 'getPhotoInfoFromFileList: disabled' });
+});
+
+router.get('/reducePhotoJSON', (req, res) => {
+    res.render('download', { title: 'Reduce all Photo JSON files to one JSON', data: unsplashHelper.reducePhotoJSON() });
+});
+
+router.get('/getRandomPhotoJson', (req, res) => {
+    res.render('download', { title: 'Reduce all Photo JSON files to one JSON', data: 'getRandomPhotoJson: disabled' });
+});
+
 
 module.exports = router;
